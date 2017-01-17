d51439b7faebf94b14679fe0d184ef8d8dcb395e
brettmcclintock <brett.mcclintock@noaa.gov>
2017-01-17 11:05:56 -0800

    added CRAWLwrap and MIfitHMM for multiple imputation based on CTCRW model of Johnson et al. 2008

---
+++ R/CRAWLwrap.R
@@ -0,0 +1,114 @@
+#' @export
+#' @importFrom sp coordinates
+#' @importFrom crawl crwMLE crwPredict crwSimulator
+#' @importFrom doParallel registerDoParallel stopImplicitCluster
+#' @importFrom foreach foreach %dopar%
+
+CRAWLwrap<-function(obsData, timeStep=1, ncores, 
+                    mov.model = NULL, err.model = NULL, activity = NULL, drift = NULL, 
+                    coord = c("x", "y"), Time.name = "time", initial.state, theta, fixPar, 
+                    methodMLE = "L-BFGS-B", control = NULL, constr = NULL, 
+                    prior = NULL, need.hess = TRUE, initialSANN = list(maxit = 200), attempts = 1,
+                    predTime = NULL, 
+                    methodSim = "IS", parIS = 1000, df = Inf, grid.eps = 1, crit = 2.5, scale = 1, force.quad)
+{
+  
+  if(any(!(c("ID",Time.name,coord) %in% names(obsData)))) stop('obsData is missing ',paste(c("ID",Time.name,coord)[!(c("ID",Time.name,coord) %in% names(obsData))],collapse=","))
+  
+  toProj = obsData[!is.na(obsData$y),c("ID",Time.name,coord)]
+  sp::coordinates(toProj) = formula(paste0("~",paste0(coord,collapse="+")))
+  
+  ids = unique(obsData$ID)
+  ind_data<-vector('list',length(ids))
+  
+  for(i in 1:length(ids)){
+    ind_data[[i]] = base::subset(obsData,ID == ids[i])
+  }
+  
+  if(is.null(mov.model)){
+    mov.model<-vector('list',length(ids))
+    for(i in 1:length(ids)){
+      mov.model[[i]] <- ~1
+    }
+  }
+  
+  if(is.null(err.model)){
+    err.model<-vector('list',length(ids))
+  }
+  
+  if(is.null(activity)){
+    activity<-vector('list',length(ids))
+  }
+  
+  if(is.null(drift)){
+    drift<-vector('list',length(ids))
+    for(i in 1:length(ids)){
+      drift[[i]] <- FALSE
+    }
+  }
+  
+  if(is.null(constr)){
+    constr<-vector('list',length(ids))
+    for(i in 1:length(ids)){
+      constr[[i]]<-list(lower = -Inf,upper = Inf)
+    }
+  }
+  
+  if(is.null(prior)){
+    prior<-vector('list',length(ids))
+  }
+  
+  registerDoParallel(cores=ncores) 
+  model_fits <- 
+    foreach(i = 1:length(ids)) %dopar% {
+      
+      fit <- crawl::crwMLE(
+        mov.model =  mov.model[[i]],
+        err.model = err.model[[i]],
+        activity = activity[[i]],
+        drift = drift[[i]],
+        data = ind_data[[i]],
+        coord = coord,
+        Time.name = Time.name,
+        initial.state = initial.state[[i]],
+        theta = theta[[i]],
+        fixPar = fixPar[[i]],
+        method = methodMLE,
+        control = control,
+        constr = constr[[i]],
+        prior = prior[[i]],
+        need.hess = need.hess,
+        initialSANN = initialSANN,
+        attempts = attempts
+      )
+      
+    }
+  stopImplicitCluster()
+  
+  if(is.null(predTime)){
+    predTime<-vector('list',length(ids))
+    for(i in 1:length(ids)){
+      iTime <- range(subset(obsData,ID==i)[[Time.name]])
+      predTime[[i]] <- seq(ceiling(iTime[1]),floor(iTime[length(iTime)]),timeStep)
+    }
+  }
+  
+  registerDoParallel(cores=ncores)
+  predData <- foreach(i = 1:length(ids), .combine = rbind) %dopar% {
+    mf<-model_fits[[i]]
+    if(!is.null(err.model)) mf$rho[is.na(mf$rho)]<-0
+    tmp = crawl::crwPredict(mf, predTime=predTime[[i]])
+  }
+  stopImplicitCluster()
+  
+  registerDoParallel(cores=ncores)
+  crwSim <- foreach(i = 1:length(model_fits)) %dopar% {
+    mf<-model_fits[[i]]
+    if(!is.null(err.model)) mf$rho[is.na(mf$rho)]<-0
+    tmp = crawl::crwSimulator(mf,predTime=predData[[Time.name]][which(predData$ID==i & predData$locType=="p")], method = methodSim, parIS = parIS,
+                              df = df, grid.eps = grid.eps, crit = crit, scale = scale, force.quad)
+  }
+  stopImplicitCluster()
+  
+  return(list(model_fits=model_fits,predData=predData,crwSim=crwSim))
+}
---
+++ R/MIfitHMM.R
@@ -0,0 +1,36 @@
+#' @export
+#' @importFrom crawl crwPostIS
+#' @importFrom doParallel registerDoParallel stopImplicitCluster
+#' @importFrom foreach foreach %dopar%
+MIfitHMM<-function(n,ncores,obsData,crwFits,
+                   nbStates, dist, Par, beta0 = NULL, delta0 = NULL,
+                   estAngleMean = NULL, formula = ~1, stationary = FALSE, verbose = 0,
+                   nlmPar = NULL, fit = TRUE, DM = NULL, cons = NULL,
+                   userBounds = NULL, workcons = NULL, stateNames = NULL,
+                   type=c("LL", "UTM"),coordNames=c("x","y"),covNames=NULL,spatialCovs=NULL,
+                   fullPost = TRUE, df = Inf, scale = 1,thetaSamp = NULL){
+  
+  nbAnimals<-length(crwFits$model_fits)
+  
+  registerDoParallel(cores=ncores)
+  multipleFits <-
+    foreach(j = 1:n, .export=c("crwPostIS","prepData","fitHMM")) %dopar% {
+      locs<-data.frame()
+      locType<-character()
+      for(i in 1:nbAnimals){
+        tmp<-crawl::crwPostIS(crwFits$crwSim[[i]], fullPost, df = df, scale = scale, thetaSamp = thetaSamp)
+        locs<-rbind(locs,tmp$alpha.sim[,c("mu.x","mu.y")])
+        locType<-c(locType,tmp$locType)
+      }
+      predData<-list(mu.x=locs$mu.x,mu.y=locs$mu.y,locType=locType)
+      distnames<-names(dist)[which(!(names(dist) %in% c("step","angle")))]
+      mh<-data.frame(x=predData$mu.x,y=predData$mu.y,obsData[,c("ID",distnames),drop=FALSE])[which(predData$locType=="p"),]
+      mh<-momentuHMM:::prepData(mh,type=type,coordNames=coordNames,covNames=covNames,spatialCovs=spatialCovs)
+      fit<-momentuHMM::fitHMM(mh,nbStates, dist, Par, beta0, delta0,
+                              estAngleMean, formula, stationary, verbose,
+                              nlmPar, fit, DM, cons,
+                              userBounds, workcons, stateNames)
+    }  
+  stopImplicitCluster()
+  multipleFits
+}
