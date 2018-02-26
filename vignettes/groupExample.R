a3347b0657ca633197c0920e4cd5a862ab9f9df6
brettmcclintock <brett.mcclintock@noaa.gov>
2018-02-26 13:52:52 -0800

    update groupExample

--- vignettes/groupExample.R
+++ vignettes/groupExample.R
@@ -15,6 +15,7 @@ Parc <- list(step=c(15,10), angle = c(0.15,log(1)))
 DMc <- list(angle=list(mean=~center1.angle,concentration=~1))
 
 centroidData <- simData(nbStates=1,dist=dist,Par=Parc,DM=DMc,circularAngleMean=list(angle=TRUE),centers=matrix(0,1,2),obsPerAnimal = nbObs)
+centroidData$ID <- "centroid"
 plot(centroidData,ask=FALSE)
 #########################################################################
 
