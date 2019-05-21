31034e876bf21d6d4362269fca0fd924334b13b6
Brett McClintock <brett.mcclintock@noaa.gov>
2019-05-21 13:51:08 -0700

    revert to crawl (>= 2.2.1)

--- tests/testthat/test_MIfitHMM.R
+++ tests/testthat/test_MIfitHMM.R
@@ -4,7 +4,7 @@ test_that("Exceptions are thrown",{
   
   oldRNG<-setRNG::setRNG()
   
-  setRNG::setRNG(kind="L'Ecuyer-CMRG",normal.kind="Inversion",seed=2)
+  setRNG::setRNG(kind="L'Ecuyer-CMRG",normal.kind="Inversion",seed=1)
   
   theta<-c(6,-0.5)
   err.model <- list(x= ~ ln.sd.x - 1, y =  ~ ln.sd.y - 1, rho =  ~ error.corr)
@@ -30,7 +30,7 @@ test_that("betaCons works correctly",{
   
   oldRNG<-setRNG::setRNG()
   
-  setRNG::setRNG(kind="L'Ecuyer-CMRG",normal.kind="Inversion",seed=3)
+  setRNG::setRNG(kind="L'Ecuyer-CMRG",normal.kind="Inversion",seed=1)
   
   theta<-c(6,-0.5)
   err.model <- list(x= ~ ln.sd.x - 1, y =  ~ ln.sd.y - 1, rho =  ~ error.corr)
