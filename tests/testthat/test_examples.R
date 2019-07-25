88a43577aa5ee07f5e10b6a3ce9b1205565575eb
brettmcclintock <brett.mcclintock@noaa.gov>
2019-07-25 14:17:21 -0700

    devtools has mixed up the logic for 'dontrun' examples; until this is fixed, use devtools::run_examples(run = TRUE)

--- tests/testthat/test_examples.R
+++ tests/testthat/test_examples.R
@@ -3,7 +3,7 @@ context("examples")
 
 test_that("all examples work correctly",{
 
-  expect_error(devtools::run_examples(run=FALSE),NA)
+  expect_error(devtools::run_examples(run=TRUE),NA)
   
   unlink(paste0("Rplots",1:47,".pdf"))
 })
