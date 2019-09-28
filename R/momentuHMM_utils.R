746e6e825d515d3ff815e42623dfbddc4120cb21
brettmcclintock <brett.mcclintock@noaa.gov>
2019-09-28 13:41:55 -0700

    maintain backwards compatibility for older models that are missing 'conditions$fit'

--- R/momentuHMM_utils.R
+++ R/momentuHMM_utils.R
@@ -140,8 +140,9 @@ muffleRNGwarning <- function(w) {
 delta_bc <- function(m){
   
   if(is.momentuHMM(m) | is.miSum(m)){
-    if(!m$conditions$fit) warning("The given model hasn't been fitted.")
-    else m$conditions$fit <- TRUE
+    if(!is.null(m$conditions$fit)){
+      if(!m$conditions$fit) warning("The given model hasn't been fitted.")
+    } else m$conditions$fit <- TRUE
     if(is.null(m$conditions$workBounds)){
       distnames <- names(m$conditions$dist)
       
