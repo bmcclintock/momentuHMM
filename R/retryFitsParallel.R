28b086c2865e6024f5e5d09910e91ec0c656be14
bmcclintock <brett.mcclintock@gmail.com>
2023-02-23 02:30:23 +0000

    calculate hessian for best model fit when using retryFits in parallel

--- R/retryFitsParallel.R
+++ R/retryFitsParallel.R
@@ -18,11 +18,13 @@ retryFitsParallel <- function(data,nbStates, dist, Par0, beta0, delta0,
   quietCrawl <- quietCrawl
   
   if(optMethod=="nlm"){
+    hess <- nlmPar$hessian
     tmpPar <- nlmPar
     tmpPar$hessian <- FALSE
     nlmPar <- tmpPar
     control <- NULL
   } else {
+    hess <- control$hessian
     tmpPar <- control
     tmpPar$hessian <- FALSE
     control <- tmpPar
@@ -49,13 +51,13 @@ retryFitsParallel <- function(data,nbStates, dist, Par0, beta0, delta0,
                                  
                                  progBar(j,retryFits)
                                  
-                                 quietCrawl(jFit<-suppressMessages(fitHMM(data=data,nbStates=nbStates,dist=dist,
+                                 quietCrawl(jFit<-suppressWarnings(suppressMessages(fitHMM(data=data,nbStates=nbStates,dist=dist,
                                                                           Par0=curPar$Par,beta0=curPar$beta,delta0=curPar$delta,
                                                                           estAngleMean=estAngleMean,circularAngleMean=circularAngleMean,
                                                                           formula=formula,formulaDelta=formulaDelta,stationary=stationary,mixtures=mixtures,formulaPi=formulaPi,
                                                                           nlmPar=nlmPar,fit=fit,
                                                                           DM=DM,userBounds=userBounds,workBounds=workBounds,betaCons=betaCons,betaRef=betaRef,deltaCons=deltaCons,
-                                                                          mvnCoords=mvnCoords,stateNames=stateNames,knownStates=knownStates,fixPar=fixPar,retryFits=1,retrySD=retrySD,ncores=1,optMethod=optMethod,control=control,prior=prior,modelName=modelName, ..., dontcheck=dontcheck)))
+                                                                          mvnCoords=mvnCoords,stateNames=stateNames,knownStates=knownStates,fixPar=fixPar,retryFits=1,retrySD=retrySD,ncores=1,optMethod=optMethod,control=control,prior=prior,modelName=modelName, ..., dontcheck=dontcheck))))
                                  jFit
                                } 
                              ,warning=muffleCTwarning)
@@ -63,6 +65,21 @@ retryFitsParallel <- function(data,nbStates, dist, Par0, beta0, delta0,
   if(!all(unlist(lapply(fits,function(x) inherits(x,"error"))))){
     bestInd <- which.min(unlist(lapply(fits,function(x) ifelse(!inherits(x,"error"),x$mod$minimum,Inf))))
     curmod <- fits[[bestInd]]
+    if(!isFALSE(hess)){
+      cat("Calculating hessian for best model fit...\n\n")
+      curPar <- getPar(curmod)
+      if(optMethod=="nlm"){
+        nlmPar$hessian <- TRUE
+        nlmPar$print.level <- 0
+      } else {
+        control$hessian <- TRUE
+        control$trace <- 0
+      }
+      curmod <- suppressMessages(fitHMM(data,nbStates, dist, curPar$Par, curPar$beta, curPar$delta,
+                       estAngleMean, circularAngleMean, formula, formulaDelta, stationary, mixtures, formulaPi,
+                       nlmPar=nlmPar, fit, DM,
+                       userBounds, workBounds, betaCons, betaRef, deltaCons, mvnCoords, stateNames, knownStates, fixPar, retryFits=0, retrySD, ncores=1, optMethod, control=control, prior, modelName, ...))
+    }
   } else warning("all retryFits failed")
   cat("Final log-likelihood value: ",-curmod$mod$minimum,"\n",sep="")
   return(curmod)
