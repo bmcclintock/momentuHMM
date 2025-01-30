20385b4078e04e6604c1225c2513489b9ae28fa7
Brett McClintock <brett.mcclintock@noaa.gov>
2025-01-29 16:15:34 -0800

    simplify ctcrw variance conversion

--- R/momentuHMM_utils.R
+++ R/momentuHMM_utils.R
@@ -344,8 +344,7 @@ convertCTCRW <- function(distname,par,nbStates,data){
   for(i in 1:nbStates){
     mvnpar[i,] <- data[[paste0(distname,".x_tm1")]] + par[i,]^dtm1 * crw(data[[paste0(distname,".x_tm1")]],dt=data$dt)
     mvnpar[nbStates+i,] <- data[[paste0(distname,".y_tm1")]] + par[i,]^dtm1 * crw(data[[paste0(distname,".y_tm1")]],dt=data$dt)
-    var <- (-log(par[i,])*par[nbStates+i,]^2)/(2*log(par[i,])^2)
-    mvnpar[2*nbStates+i,] <- mvnpar[3*nbStates+i,] <- sqrt(var-par[i,]^data$dt * var * par[i,]^data$dt)
+    mvnpar[2*nbStates+i,] <- mvnpar[3*nbStates+i,] <- sqrt(par[nbStates+i,]^2*(par[i,]^(2*data$dt)-1)/(2*log(par[i,])))
   }
   return(mvnpar)
 }
