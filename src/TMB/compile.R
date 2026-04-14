35017ccd341ac11359b5e3a4c19e71054384d11b
Brett McClintock <brett.mcclintock@gmail.com>
2026-04-14 16:55:33 -0700

    compile using TMBad

--- src/TMB/compile.R
+++ src/TMB/compile.R
@@ -3,9 +3,15 @@ tmb_flags <- commandArgs(trailingOnly = TRUE)
 
 if(file.exists(paste0(tmb_name, ".cpp"))) {
   if(length(tmb_flags) == 0) tmb_flags <- ""
+  
+  tmb_flags <- paste(tmb_flags, "-g0")
+  
+  options(tmb.ad.framework = "TMBad")
+  
   TMB::compile(file = paste0(tmb_name, ".cpp"),
                PKG_CXXFLAGS = tmb_flags,
                safebounds = FALSE, safeunload = FALSE)
+  
   file.copy(from = paste0(tmb_name, .Platform$dynlib.ext),
             to = "..", overwrite = TRUE)
 }
